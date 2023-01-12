b0c4d569c544ffa51500de4d633d1bcd86ea7cef
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-11 21:43:06 -0300

    update Inicial

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(faPhone);
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="window">
+        <h1>My TASKs <font-awesome-icon icon="fa-regular fa-book-open" /></h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView'
+}
+</script>
+
+<style scoped>
+    #bookIcon{
+        color: #fff;
+    }
+    h1{
+        color: #fff;
+    }
+    #window{
+        background-color: black;
+        height: 100vh;
+        width: 100%;
+    }
+</style>
