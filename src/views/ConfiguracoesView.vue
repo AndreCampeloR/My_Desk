10b5e93549dbf76d71fd1b11d89efe1c0990b5d0
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-12 21:42:45 -0300

    creating ConfigView

--- src/views/ConfiguracoesView.vue
+++ src/views/ConfiguracoesView.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="ConfiguracaoView">
-    <Nav/>
-    <Footer/>
+
   </div>
 </template>
 
 <script>
-import Footer from '../components/Footer.vue'
-import Nav from '../components/Nav.vue'
+
 export default {
     name: "ConfiguracoesView",
     components: {
-      Footer,
-      Nav
+
     }
 }
 </script>
