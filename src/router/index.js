3cf96c745439aa837d5fe0de03d03ab549726170
AndreCampeloR <decocampeloreis@gmail.com>
2023-01-13 10:24:02 -0300

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LoginPage from '../views/LoginView.vue'
+import Tasks from '../views/TasksView.vue'
 
 const routes = [
   {
     path: '/login',
     name: 'login',
     component: LoginPage
+  },
+  {
+    path: '/',
+    name: 'Tasks',
+    component: Tasks
   }
 ]
 
