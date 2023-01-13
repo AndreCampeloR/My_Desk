446a93390e2789fc8cb64b8f810079e0a42c4727
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-12 21:10:32 -0300

    update

---
+++ src/views/ConfiguracoesView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="ConfiguracaoView">
+    <h1>TESTE CONFIG</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ConfiguracoesView"
+}
+</script>
+
+<style>
+
+</style>
