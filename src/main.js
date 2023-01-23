c70171c5f40ad031df426a2a2e321a24d5c8b788
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2023-01-23 15:50:47 -0300

    update

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://localhost:7172/';
+
 createApp(App).use(store).use(router).mount('#app')
