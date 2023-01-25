c4ab2117c72cd85d2486900eae83073c0ad0e95c
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2023-01-25 10:07:54 -0300

    ignorando https

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import https from 'https'
 
 
+axios.defaults.httpsAgent = new https.Agent({  
+    rejectUnauthorized: false
+});
 const api = axios.create({
     baseURL: "https://localhost:7172/"
 });
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  resolve: {
+    fallback: {
+      'https': require.resolve('https-browserify')
+    }
+}
+
 })
