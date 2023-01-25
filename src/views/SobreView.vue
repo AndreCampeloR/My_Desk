cb226b9b14a32e6a69c12a9b6b52e3c2748b8ad1
AndreCampeloR <decocampeloreis@gmail.com>
2023-01-25 10:28:20 -0300

    add img zenToDone

--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -2,11 +2,23 @@
   <div class="main-container">
     <Nav/>
     <main>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      Aperiam odio nemo laboriosam hic 
-      assumenda dicta expedita repellat ipsum eligendi sed corrupti,
-      fugiat excepturi incidunt? Quasi veniam 
-      aliquam nostrum fuga quia!</p>
+
+    <section id="sobre">
+      <h2>Sobre o Projeto</h2>
+         <div>
+              <p>O my Desk é um gerenciador de produtividade baseado no método 'Zen To Done'.</p>
+              <p>As tarefas são divididas em 2 grupos, Normal Tasks e Danger Tasks (Tarefas Emergenciais).</p>
+              <p>As tasks normais são organizadas em 2 subgrupos, Main tasks (As 3 tarefas de maior prioridade) e as tasks secundarias (demais tarefas com menor prioridade).</p>
+              <p>As danger tasks são tarefas que surgem de forma imprevista se assumindo um caráter emergencial.</p>
+          </div>
+    </section>
+
+    <section id="estudo">
+      <h2>O estudo</h2>
+      <p></p>
+      <img src="/img/diagrama.jpg" alt="">
+    </section>
+    <section id="detalhes"></section>
     </main>
     <Footer/>
   </div>
@@ -27,5 +39,24 @@ export default {
 </script>
 
 <style scoped>
+
+h2{
+  font-size: 1.5rem;
+}
+
+p{
+  font-size: 1.0rem;
+  margin: 10px;
+}
+
+
+#sobre{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 15px auto;
+}
+
 
 </style>
